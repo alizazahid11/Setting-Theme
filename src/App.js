05571c56489e1aa1f094debe6c2ae8f852126700
aliza zahid <alizayzah11@gmail.com>
2023-10-04 17:46:36 +0500

    setting the navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import './App.css'
+import {
+  Nav,
+  NavItem,
+  Dropdown,
+  DropdownItem,
+  DropdownToggle,
+  DropdownMenu,
+  NavLink,
+} from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+function Example(props) {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-function App() {
+  const toggle = () => setDropdownOpen(!dropdownOpen);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <Nav >
+      <NavItem>
+        <NavLink href="#"className='material'>
+          Material Kit React
+        </NavLink>
+      </NavItem>
+      <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
+        <DropdownToggle nav caret className='Components'>
+          Components
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Header</DropdownItem>
+          <DropdownItem disabled>Action</DropdownItem>
+          <DropdownItem>Another Action</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Another Action</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      <NavItem>
+        <NavLink href="#" className='Upgrade' >Upgrade To Pro</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#"className='Download'>Download</NavLink>
+      </NavItem>
+      <NavItem>
+      <div className="social-icons">
+      <a href="https://www.instagram.com" className='social-icon'>
+      <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+      <a href="https://www.facebook.com"className='social-icon'>
+      <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      <a href="https://www.twitter.com"className='social-icon'>
+      <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
     </div>
+      </NavItem>
+    </Nav>
   );
 }
 
-export default App;
+export default Example;
